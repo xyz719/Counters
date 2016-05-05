c6e3be9b36f92f9c13a98e5704d3873c635c5433
xyz719 <a20131105808@mac23.local>
2016-05-05 11:43:58 +0800

    update

--- Counters/Bridging-Header.h
+++ Counters/Bridging-Header.h
@@ -5,11 +5,5 @@
 //  Created by 20131105808 on 16/4/21.
 //  Copyright © 2016年 20131105808. All rights reserved.
 //
-
-#ifndef Bridging_Header_h
-#define Bridging_Header_h
-
-
-#endif /* Bridging_Header_h */
 #import "sqlite3.h"
 #import <time.h>
