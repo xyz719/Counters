d410046a5d84483968fa5e08549292570450ec24
xyz719 <a20131105808@mac23.local>
2016-05-16 20:20:13 +0800

    add a date to save team name
    add a date to save team name

--- Counters/ViewController.swift
+++ Counters/ViewController.swift
@@ -12,8 +12,8 @@ class ViewController: UIViewController {
     
     var time:NSTimer!//建立一个计时器
     var db:SQLiteDB!
-    @IBOutlet var txtzhudui: UITextField!
-    @IBOutlet var txtkedui: UITextField!
+    @IBOutlet weak var txtzhudui: UITextField!
+    @IBOutlet weak var txtkedui: UITextField!
     var a=0
     var b=0
     var x=12//分钟
@@ -25,6 +25,10 @@ class ViewController: UIViewController {
     }
     @IBOutlet weak var la2: UILabel!
     @IBOutlet weak var la1: UILabel!
+    
+    @IBOutlet weak var team1: UITextField!
+    @IBOutlet weak var team2: UITextField!
+    
     @IBAction func two(sender: UIButton) {
         a=a+2
         txtzhudui.text=("\(a)")
@@ -62,16 +66,17 @@ class ViewController: UIViewController {
         db = SQLiteDB.sharedInstance()
         //如果表还不存在则创建表（其中uid为自增主键）
         db.execute("create table if not exists t_user(uid integer primary key,uname varchar(20),mobile varchar(20))")
-        db.execute("create table if not exists t_team(uid integer primary key,team1 varchar(20),team2 varchar(20))")
-        
-        //如果有数据则加载
         initUser()
+        
+        db.execute("create table if not exists t_team(uid integer primary key,team1 varchar(20),team2 varchar(20))")
+        //如果有数据则加载
+        initteam()
     }
     
     //点击保存
     @IBAction func saveClicked(sender: AnyObject) {
-        saveUser()
         saveteam()
+        
     }
     
     //从SQLite加载数据
@@ -88,20 +93,21 @@ class ViewController: UIViewController {
         
         
     }
-    func initUser() {
-        let data = db.query("select * from t_user")
+    
+    func initteam() {
+        let data = db.query("select * from t_team")
         if data.count > 0 {
             //获取最后一行数据显示
-            let user = data[data.count - 1]
-            txtzhudui.text = user["uname"] as? String
-            txtkedui.text = user["mobile"] as? String
+            let team = data[data.count - 1]
+            team1.text = team["team1"] as? String
+            team2.text = team["team2"] as? String
         }
         //
         
         
         
     }
-    
+
     //保存数据到SQLite
     func saveteam() {
         let team1 = self.team1.text!
@@ -114,6 +120,20 @@ class ViewController: UIViewController {
         
         //通过封装的方法执行sql
         let result = db.execute(sql)
+        print(result)
+    }
+    
+    func saveUser() {
+        let uname = self.txtzhudui.text!
+        let mobile = self.txtkedui.text!
+        //插入数据库，这里用到了esc字符编码函数，其实是调用bridge.m实现的
+        let sql1 = "insert into t_user(uname,mobile) values('\(uname)','\(mobile)')"
+        print("sql1: \(sql1)")
+        
+        
+        
+        //通过封装的方法执行sql
+        let result = db.execute(sql1)
         print(result)
     }
     
